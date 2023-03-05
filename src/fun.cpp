c3036e668dd6825e015e918a225f1cc2bceb4a1a
German (forodie) <german.utkingu@gmail.com>
2023-03-05 22:46:14 +0300

    test

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,12 +1,7 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <math.h>
 
 int64_t power(int64_t x, uint16_t n) {
-    int64_t z = 1;
-
-    for (std::size_t i = 0; i < n; i++) {
-        z *= x;
-    }
-
-    return z;
+    return pow(x, n);
 }
