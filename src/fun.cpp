f82835f4e76c8fa3ca49bb37f0723d6acc18a722
German (forodie) <german.utkingu@gmail.com>
2023-03-05 22:51:28 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,7 +1,9 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
-#include <math.h>
 
 int64_t power(int64_t x, uint16_t n) {
-    return pow(x, n);
+    if (n == 0) {
+        return 1;
+    }
+    return x * power(x, n-1);
 }
